9a3704d21e7f72f4a1293e0e20c7c4886a2bc696
MohammedIZaki <zakiz0054@gmail.com>
2021-03-11 10:12:52 +0200

    1- refactor

--- build/js/custom.js
+++ build/js/custom.js
@@ -33,21 +33,33 @@
     jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
 
 })(jQuery,'smartresize');
-$(".fixed-menu button.dropdown-toggle").on("click", function (event) {
-    $(this).parent().toggleClass("open");
-    $(".menu").toggleClass("open");
-    console.log("clicked")
-});
+// $(".fixed-menu button.dropdown-toggle").on("click", function (event) {
+//     $(this).parent().toggleClass("open");
+//     $(".menu").toggleClass("open");
+//     console.log("clicked")
+// });
+//
+// $("body").on("click", function (e) {
+//     if (!$('li.dropdown.mega-dropdown').is(e.target)
+//         && $('li.dropdown.mega-dropdown').has(e.target).length === 0
+//         && $('.open').has(e.target).length === 0
+//     ) {
+//         $('.fixed-menu ').removeClass('open');
+//         $('.menu ').removeClass('open');
+//     }
+// });
 
-$("body").on("click", function (e) {
-    if (!$('li.dropdown.mega-dropdown').is(e.target)
-        && $('li.dropdown.mega-dropdown').has(e.target).length === 0
-        && $('.open').has(e.target).length === 0
-    ) {
-        $('.fixed-menu ').removeClass('open');
-        $('.menu ').removeClass('open');
-    }
-});
+$(document)
+    .on("click", '.fixed-menu button.menu-toggle', function(event) {
+        $(this).parent().toggleClass("open");
+        $(".menu").toggleClass("open");
+    })
+    .on("click", "body", function(e) {
+        if ($('.open').has(e.target).length === 0) {
+            $('.fixed-menu').removeClass('open');
+            $('.menu').removeClass('open');
+        }
+    })
 
 /**
  * To change this license header, choose License Headers in Project Properties.
